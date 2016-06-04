570a4f5cfb6afd4649c633061a78f4d891966ebb
Ralph <ralph_mct@yahoo.co.uk>
2016-06-04 15:58:33 +0100

    Format

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,10 +1,10 @@
 package hello;
 
-        import java.util.Arrays;
+import java.util.Arrays;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.context.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
