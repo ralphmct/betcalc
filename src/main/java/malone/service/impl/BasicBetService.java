9b1fa6e799dc369ebdaa9b1338668988cf478d1e
simonstorey94 <storeysimon2@gmail.com>
2016-06-14 15:06:49 +0100

    added a calculation to calculate the return. Having difficulty running it on my laptop so haven't checked if it's working.

--- src/main/java/malone/service/impl/BasicBetService.java
+++ src/main/java/malone/service/impl/BasicBetService.java
@@ -2,20 +2,22 @@ package malone.service.impl;
 
 import malone.service.BetService;
 import org.springframework.stereotype.Component;
+import java.util.Scanner;
 
 /**
  * Created by mct on 10/06/16.
  */
 @Component
 public class BasicBetService implements BetService {
-
+    Scanner input = new Scanner(System.in);
     @Override
     public String calculateBet(int oddsPart1, int oddsPart2, int stake) {
         return "Your bet profit is: " + calculateProfit(oddsPart1, oddsPart2, stake);
     }
 
-    private double calculateProfit(int oddsPart1, int oddsPart2, int stake) {
 
-        return 0.0;
+    private double calculateProfit(int oddsPart1, int oddsPart2, int stake) {
+        double calculateProfit = (stake * oddsPart1 / oddsPart2) + stake;
+        return calculateProfit;
     }
 }
